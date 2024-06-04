6115fda6a9b8b8da636fd61463a34952eed72e57
ndraai084 <abril.adrian084@gmail.com>
2024-06-04 19:35:40 +0800

    second commit

--- slider.js
+++ slider.js
@@ -1,5 +1,31 @@
-const card = document.querySelector(".slider_card");
+const card1 = document.querySelector("#slider_card_0");
+const card2 = document.querySelector("#slider_card_1");
+const card3 = document.querySelector("#slider_card_2");
 
-card.addEventListener("click", () => {
-    card.classList.toggle("clicked");
-});
+let cards = [];
+
+card1.addEventListener("click", () => {
+    card1.classList.toggle("clicked");
+    cards = [card2, card3];
+    refreshCard();
+})
+
+card2.addEventListener("click", () => {
+    card2.classList.toggle("clicked");
+    cards = [card1, card3];
+    refreshCard();
+})
+
+card3.addEventListener("click", () => {
+    card3.classList.toggle("clicked");
+    cards = [card1, card2];
+    refreshCard();
+})
+
+function refreshCard(){
+    for(let c of cards){
+        c.classList.remove("clicked");
+    }
+
+    cards = [];
+}
